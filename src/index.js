76aba404310ac07bc2955c6b284d968c5da68008
jvsr18 <jvsr.99@hotmail.com>
2022-07-20 18:36:01 -0300

    chore(index / auth route): definição de rota para login

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ const cors = require("cors");
 const port = process.env.PORT || 3001;
 const app = express();
 const userRoute = require("./routes/users.route");
+const authRoute = require("./routes/auth.route");
 const dbConnection = require("./database/db");
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/users", userRoute);
+app.use("/auth", authRoute);
 
 dbConnection();
 
