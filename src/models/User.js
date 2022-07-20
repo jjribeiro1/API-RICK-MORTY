c0393d2bd5f603d6fe531a0d655e719b0061513f
jvsr18 <jvsr.99@hotmail.com>
2022-07-20 18:03:26 -0300

    chore(user model): encriptação de senha do usuario com bcrypt

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -24,6 +25,16 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    this.password = await bcrypt.hash(this.password, 10);
+    return next();
+  } catch (err) {
+    return next(err);
+  }
 });
 
 const User = mongoose.model("User", UserSchema, "users");
