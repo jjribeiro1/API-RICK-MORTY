a3a6a0ea29d8767d2e4542296952b528dc82cfef
jvsr18 <jvsr.99@hotmail.com>
2022-07-21 18:31:06 -0300

    chore(characters controller / service): definição das funções dos characters

---
+++ src/controllers/characters.controller.js
@@ -0,0 +1,22 @@
+const charactersService = require("../services/characters.service");
+
+const createCharacterController = async (req, res) => {};
+
+const readAllCharacterController = async (req, res) => {};
+
+const readCharacterByIdController = async (req, res) => {};
+
+const readCharacterByNameController = async (req, res) => {};
+
+const updateCharacterController = async (req, res) => {};
+
+const deleteCharacterController = async (req, res) => {};
+
+module.exports = {
+  createCharacterController,
+  readAllCharacterController,
+  readCharacterByIdController,
+  readCharacterByNameController,
+  updateCharacterController,
+  deleteCharacterController,
+};
---
+++ src/services/characters.service.js
@@ -0,0 +1,22 @@
+const Character = require("../models/Character");
+
+const createCharacterService = async () => {};
+
+const readAllCharacterService = async () => {};
+
+const readCharacterByIdService = async () => {};
+
+const readCharacterByNameService = async () => {};
+
+const updateCharacterService = async () => {};
+
+const deleteCharacterService = async () => {};
+
+module.exports = {
+  createCharacterService,
+  readAllCharacterService,
+  readCharacterByIdService,
+  readCharacterByNameService,
+  updateCharacterService,
+  deleteCharacterService,
+};
