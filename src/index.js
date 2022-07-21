254696f8b52f6e4cc4d8e299b371f4971b4df344
jvsr18 <jvsr.99@hotmail.com>
2022-07-21 18:17:28 -0300

    fix(index): correção de nome /characters

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use("/users", userRoute);
 app.use("/auth", authRoute);
-app.use("characters", charactersRoute);
+app.use("/characters", charactersRoute);
 
 dbConnection();
 
