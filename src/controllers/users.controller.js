e2d30ec91ffa2034ece344450a130684729b5f08
jvsr18 <jvsr.99@hotmail.com>
2022-07-21 00:09:22 -0300

    style(users): formatação

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -10,6 +10,7 @@ const createUserController = async (req, res) => {
     if (!response) {
       return res.status(500).send({ message: "Error creating user" });
     }
+
     return res
       .status(201)
       .send({ data: response, message: "User created successfully" });
@@ -20,11 +21,11 @@ const createUserController = async (req, res) => {
 
 const readAllUsersController = async (req, res) => {
   try {
-    const response = await usersService.readAllUsersService()
-    if(response.length === 0) {
-      return res.status(404).send({ message: "No user has been registered"})
+    const response = await usersService.readAllUsersService();
+    if (response.length === 0) {
+      return res.status(404).send({ message: "No user has been registered" });
     }
-    return res.status(200).send({data: response})
+    return res.status(200).send({ data: response });
   } catch (err) {
     return res.status(500).send({ message: err.message });
   }
--- src/services/users.service.js
+++ src/services/users.service.js
@@ -5,6 +5,6 @@ const createUserService = async (user) => await User.create(user);
 const readAllUsersService = async () => await User.find();
 
 module.exports = {
-    createUserService,
-    readAllUsersService
+  createUserService,
+  readAllUsersService,
 };
