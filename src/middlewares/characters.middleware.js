c2776e355de1f62234658cc3ab94dc8b659137e4
jvsr18 <jvsr.99@hotmail.com>
2022-07-22 16:53:21 -0300

    chore(characters middleware): middleware que verifica se existe um character com o id recebido

--- src/middlewares/characters.middleware.js
+++ src/middlewares/characters.middleware.js
@@ -1,9 +1,18 @@
 const mongoose = require("mongoose");
+const Character = require("../models/Character");
 
 const idValidation = (req, res, next) => {
   const id = req.params.id;
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(400).send({ message: "Invalid id" });
+  }
+  next();
+};
+const notFound = async (req, res, next) => {
+  const idCharacter = req.params.id;
+  const character = await Character.findById(idCharacter);
+  if (!character) {
+    return res.status(400).send({ message: "Character not found" });
   }
   next();
 };
@@ -19,4 +28,5 @@ const bodyValidation = (req, res, next) => {
 module.exports = {
   idValidation,
   bodyValidation,
+  notFound
 };
