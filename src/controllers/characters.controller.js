47a77f67007305cd9fb4a7b58288f7f502d7695a
jvsr18 <jvsr.99@hotmail.com>
2022-07-21 23:21:09 -0300

    refactor(characters controller): colocando try e catch na leitura de todos os characters

--- src/controllers/characters.controller.js
+++ src/controllers/characters.controller.js
@@ -19,11 +19,15 @@ const createCharacterController = async (req, res) => {
 };
 
 const readAllCharacterController = async (req, res) => {
-  const response = await charactersService.readAllCharacterService();
-  if (response.length === 0) {
-    return res.status(404).send({ message: "No characters found" });
+  try {
+    const response = await charactersService.readAllCharacterService();
+    if (response.length === 0) {
+      return res.status(404).send({ message: "No characters found" });
+    }
+    return res.status(200).send({ results: response });
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
   }
-  return res.status(200).send({ results: response });
 };
 
 const readCharacterByIdController = async (req, res) => {};
