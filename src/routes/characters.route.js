e30f0fa1b1b13f22a4a7abcc59aca3bcf054d1a7
jvsr18 <jvsr.99@hotmail.com>
2022-07-21 22:23:28 -0300

    chore(characters route): adicionando middleware nas rotas de characters

--- src/routes/characters.route.js
+++ src/routes/characters.route.js
@@ -1,11 +1,44 @@
 const router = require("express").Router();
 const charactersController = require("../controllers/characters.controller");
+const {
+  idValidation,
+  bodyValidation,
+} = require("../middlewares/characters.middleware");
+const authMiddleware = require("../middlewares/auth.middleware");
 
-router.post("/create", charactersController.createCharacterController);
-router.get("/", charactersController.readAllCharacterController);
-router.get("/find/:id", charactersController.readCharacterByIdController);
-router.get("/search", charactersController.readCharacterByNameController);
-router.put("/update/:id", charactersController.updateCharacterController);
-router.delete("/delete/:id", charactersController.deleteCharacterController);
+router.post(
+  "/create",
+  authMiddleware,
+  bodyValidation,
+  charactersController.createCharacterController
+);
+router.get(
+  "/",
+  authMiddleware,
+  charactersController.readAllCharacterController
+);
+router.get(
+  "/find/:id",
+  authMiddleware,
+  idValidation,
+  charactersController.readCharacterByIdController
+);
+router.get(
+  "/search",
+  authMiddleware,
+  charactersController.readCharacterByNameController
+);
+router.put(
+  "/update/:id",
+  authMiddleware,
+  idValidation,
+  charactersController.updateCharacterController
+);
+router.delete(
+  "/delete/:id",
+  authMiddleware,
+  idValidation,
+  charactersController.deleteCharacterController
+);
 
 module.exports = router;
